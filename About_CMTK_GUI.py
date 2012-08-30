0b140d4bdc9c345e548cf79c3f6d6b57147acecd
Gregory Jefferis <jefferis@gmail.com>
2012-08-30 22:11:18 +0100

    Make it clear that CMTK/Torsten are not supporting/developing CMTK GUI!

--- About_CMTK_GUI.py
+++ About_CMTK_GUI.py
@@ -2,7 +2,8 @@ from ij.gui import GenericDialog
 import cmtkgui
 
 gd = GenericDialog('About CMTK GUI')
-gd.addMessage('CMTK GUI is open source software released under GPLv3',)
+gd.addMessage('CMTK GUI is open source software released under GPLv3')
+gd.addMessage('CMTK GUI is independently developed by Greg Jefferis and is NOT supported by CMTK team')
 gd.setCancelLabel("CMTK GUI Web Page")
 gd.showDialog()
 if gd.wasCanceled():
